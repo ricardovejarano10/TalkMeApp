95294bd2eb5c242ddc33b34dd40ebc7a2a2dd7ea
= <rvejarano@unicauca.edu.co>
2017-10-06 21:08:31 -0500

    Aplicación que ya puede ver el perfil de los que estén en el mapa con un modal

--- src/pages/modal-perfil/modal-perfil.ts
+++ src/pages/modal-perfil/modal-perfil.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
+import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Perfil } from '../../models/perfil';
+import { PerfilPage } from '../perfil/perfil';
+import firebase from 'firebase';
 
 
 @IonicPage()
@@ -10,12 +14,24 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class ModalPerfilPage {
 
-  constructor(private view:ViewController, public navCtrl: NavController, public navParams: NavParams,) {
+  perfilDatos: FirebaseObjectObservable<Perfil>
+  imgsource: any;
+  firestore = firebase.storage();
+
+  constructor(private view:ViewController,
+              public zone: NgZone,
+              private afDatabase: AngularFireDatabase,
+              public navCtrl: NavController, public navParams: NavParams,) {
   }
 
   ionViewDidLoad() {
     const key = this.navParams.get('key');
-    console.log('Estamos en el modal: ' + key);
+    this.perfilDatos = this.afDatabase.object(`perfil/${key}`)
+    this.firestore.ref().child(`image/${key}`).getDownloadURL().then((url) =>{
+      this.zone.run(() => {
+        this.imgsource = url;
+      })
+    })
   }
 
   cerrarModal(){
